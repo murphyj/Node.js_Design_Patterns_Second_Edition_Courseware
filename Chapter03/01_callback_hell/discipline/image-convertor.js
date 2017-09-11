172899c39d63b026f16b2eeaade5a2147e9c982c
James Murphy <jmurphy84@googlemail.com>
2017-09-11 19:59:33 +0100

    Chapter 3 - Reduced level of complexity in callbacks using functions and revealing module pattern

--- Chapter03/01_callback_hell/discipline/image-convertor.js
+++ Chapter03/01_callback_hell/discipline/image-convertor.js
@@ -8,64 +8,64 @@ imageConvertor.start();
 function ImageConvertor() {
 
     const levelup = require('levelup');
-
     const db = levelup('./imagedb');
 
-    return {
-        start: () => {
-            const jobQueue = ['wat2.png', 'wat1.png'];
+    function start() {
+        const jobQueue = ['wat2.png', 'wat1.png'];
 
-            let imageName = jobQueue.pop();
-            console.log('Processing image: ', imageName);
-            Jimp.read('./img/' + imageName, function(err, image) {
-                console.log('Read image: ' + './img/' + imageName);
-                if (!err) {
-                    db.put('name-' + imageName.split('.')[0], imageName, function (err) {
-                        if (err) {
-                            console.log ('Ooooops!', err); // an I/O error writing to database
-                        } else {
-                            // resize image wat1.png
-                            image.resize(256, 256)            // resize
-                                .quality(60)                 // set JPEG quality
-                                .greyscale()                 // set greyscale
-                                .write("./img/output/" + imageName.split('.')[0] + ".jpg") // write out toe the output folder
-
-                            db.put('success-' + imageName.split('.')[0], 'true', function (err) {
-                                if (err) {
-                                    console.log ('Ooooops!', err); // an I/O error writing to success message to the database
-                                } else {
-                                    console.log ('Recording successful message for ' + imageName + ' in DB!')
-
-                                    imageName = jobQueue.pop();
-                                    console.log('Processing image: ', imageName);
-                                    Jimp.read('./img/' + imageName, function(err, image) {
-                                            console.log('Read image: ' + './img/' + imageName);
-                                            if (!err) {
-                                                db.put('name-' + imageName.split('.')[0], imageName, function (err) {
-                                                    if (err) console.log ('Ooooops!', err); // an I/O error writing to database
-
-                                                    image.resize(384, 256)            // resize
-                                                        .quality(60)                 // set JPEG quality
-                                                        .greyscale()                 // set greyscale
-                                                        .write("./img/output/" + imageName.split('.')[0] + ".jpg") // write out toe the output folder
-
-                                                    db.put('success-' + imageName.split('.')[0], 'true', function (err) {
-                                                        if (err) {
-                                                            console.log ('Ooooops!', err); // an I/O error writing to success message to the database
-                                                        } else {
-                                                            console.log ('Recording successful message for ' + imageName + ' in DB!')
-                                                        }
-                                                    });
-                                                });
-                                            }
-                                        }
-                                    );
-                                }
-                            });
-                        }
+        let imageName = jobQueue.pop();
+        convertImage(imageName, function(image) {
+            insertEntryIntoDB({key: 'name-' + imageName.split('.')[0], value: imageName.split('.')[0]}, function () {
+                resizeImage({'height': 256, 'width': 256}, image, imageName, function () {
+                    imageName = jobQueue.pop();
+                    convertImage(imageName, function () {
                     });
-                }
-            });
-        }
+                });
+            })
+        });
     }
+
+    function convertImage(imageName, callback) {
+        console.log('Processing image: ', imageName);
+
+        Jimp.read('./img/' + imageName, function(err, image) {
+            console.log('Read image: ' + './img/' + imageName);
+            if (!err) {
+                callback(image);
+            }
+        });
+    }
+
+    function resizeImage(settings, image, name, callback) {
+        const nameWithoutExtension = name.split('.')[0];
+        image.resize(settings.height, settings.width)            // resize
+            .quality(60)                 // set JPEG quality
+            .greyscale()                 // set greyscale
+            .write("./img/output/" + nameWithoutExtension + ".jpg"); // write out toe the output folder
+
+        const entry = { key: 'success-' + nameWithoutExtension, value: 'true' };
+        insertEntryIntoDB(entry, function (err) {
+            if (err) {
+                console.log ('Ooooops!', err); // an I/O error writing to success message to the database
+            } else {
+                console.log ('Recording successful message for ' + name + ' in DB!');
+
+                callback();
+            }
+        });
+    }
+
+    function insertEntryIntoDB(entry, callback) {
+        db.put(entry.key, entry.value, function (err) {
+            if (err)  {
+                console.log ('Ooooops!', err); // an I/O error writing to database
+            } else {
+                callback();
+            }
+        });
+    }
+
+    return {
+        start: start
+    };
 }
