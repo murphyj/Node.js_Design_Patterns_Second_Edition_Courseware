5c7050c8db13e075164abf60e21da1178b1ede3b
murphyj <jmurphy84@googlemail.com>
2017-11-06 21:29:09 +0000

    Finished Chp5 01 and 02 Exercises

--- Chapter05/01_read/streams-read.js
+++ Chapter05/01_read/streams-read.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const EventEmitter = require('events');
 const crypto = require('crypto');
 const fs = require('fs');
 
@@ -9,28 +8,16 @@ const hash = crypto.createHash('sha1');
 
 const writeStream = fs.createWriteStream('./images/output/dilbert.jpg');
 
-const outputHash = crypto.createHash('sha1');
-
-class WriteComplete extends EventEmitter {}
-const writeCompleted = new WriteComplete();
-
 readStream
   .on('readable', function() {
     let chunk;
     while (null !== (chunk = readStream.read())) {
-      console.log("calling update chunk");
       hash.update(chunk);
+      console.log(chunk);
       writeStream.write(chunk);
     }
   })
   .on('end', function() {
     console.log("Image input hash digest: " + hash.digest('hex'));
-    writeCompleted.emit('finished_write');
   });
-
-writeCompleted.on('finished_write', function() {
-  const readStreamOutput = fs.createReadStream('./images/output/dilbert.jpg');
-  readStreamOutput.pipe(outputHash);
-  console.log("Image output hash digest: " + outputHash.digest('hex'));
-});
 
