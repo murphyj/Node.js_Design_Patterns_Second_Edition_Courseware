6268ac506dea723633005f1f21d8a01c320eee07
murphyj <jmurphy84@googlemail.com>
2017-11-18 00:52:11 +0000

    New instructions for Chapter 01 Map (DOM Elements)

--- Chapter01/05_map_set/01_map.js
+++ Chapter01/05_map_set/01_map.js
@@ -1,12 +1,8 @@
-// Exercise 1 - Maps
-// Wheat and Chessboard problem
-// If a chessboard were to have wheat placed upon each square such that one grain were placed on the first square,
-// two on the second, four on the third, and so on (doubling the number of grains on each subsequent square),
-// how many grains of wheat would be on the chessboard at the finish?
-// For the purposes of our exercise and to prevent overflows let's just calculate the map for half of the chessboard.
-// Keys should be used to represent the number of grains on that square of the chessboard with the value being the
-// coordinate those grains are sat on.
-//
-// Columns are labelled a,b,c,d,e,f,g,h
-// Rows are labelled 1,2,3,4,5,6,7,8
-// For example the first two members of the map would be 1 => a1, 2 => b1, 4 => c1, 8 => d1 and so on...
+// Disappearing DOM
+// Imagine you have a number of DOM elements on a page and these DOM elements are stored in a map prior to being displayed as HTML.
+// Initially when the DOM elements are added to the view they are also registered to the Window object in the Map
+
+// Using the ES2016 WeakMap syntax demonstrate the use of the WeakMap by adding DOM elements to the Browser window and then removing them from the browser window later on.
+
+// Once removed from the window object those dangling DOM elements will now be cleaned up.
+// DOM elements can just be represented by a simple object for simplicities sake and not a real DOM element - same with the Window object.
