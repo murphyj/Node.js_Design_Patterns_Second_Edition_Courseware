c053eaf4a8d4e8ab5fb2d7770657185460b9fde0
murphyj <jmurphy84@googlemail.com>
2017-12-16 00:53:53 +0000

    Demos for Chapter 3

---
+++ Chapter03/demos/04_spider/Ch03_spider_demo4.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const request = require('request');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const utilities = require('./utilities');
+
+function spiderLinks(currentUrl, body, nesting, callback) {
+  if(nesting === 0) {
+    return process.nextTick(callback);
+  }
+  const links = utilities.getPageLinks(currentUrl, body);
+  if(links.length === 0) {
+    return process.nextTick(callback);
+  }
+
+  let completed = 0, hasErrors = false;
+
+  function done(err) {
+    if(err) {
+      hasErrors = true;
+      return callback(err);
+    }
+    if(++completed === links.length && !hasErrors) {
+      return callback();
+    }
+  }
+
+  links.forEach(link => {
+    spider(link, nesting - 1, done);
+  });
+}
+
+function saveFile(filename, contents, callback) {
+  mkdirp(path.dirname(filename), err => {
+    if(err) {
+      return callback(err);
+    }
+    fs.writeFile(filename, contents, callback);
+  });
+}
+
+function download(url, filename, callback) {
+  console.log(`Downloading ${url}`);
+  request(url, (err, response, body) => {
+    if(err) {
+      return callback(err);
+    }
+    saveFile(filename, body, err => {
+      if(err) {
+        return callback(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      callback(null, body);
+    });
+  });
+}
+
+function spider(url, nesting, callback) {
+  const filename = utilities.urlToFilename(url);
+  fs.readFile(filename, 'utf8', (err, body) => {
+    if(err) {
+      if(err.code !== 'ENOENT') {
+        return callback(err);
+      }
+
+      return download(url, filename, (err, body) => {
+        if(err) {
+          return callback(err);
+        }
+        spiderLinks(url, body, nesting, callback);
+      });
+    }
+
+    spiderLinks(url, body, nesting, callback);
+  });
+}
+
+spider('http://www.google.co.uk', 1, () => {});
