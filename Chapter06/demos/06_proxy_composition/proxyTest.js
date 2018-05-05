272a5c92a3a638b2896ded9350886bccaf5b58bf
James Murphy <jmurphy84@googlemail.com>
2018-05-05 16:20:56 +0100

    Add augmented proxy example for proxy composition (chp6 demo 6)

--- Chapter06/demos/06_proxy_composition/proxyTest.js
+++ Chapter06/demos/06_proxy_composition/proxyTest.js
@@ -14,3 +14,10 @@ const literalProxy = LiteralProxy(subject);
 console.log("Printing results from literal proxy...");
 console.log(literalProxy.hello());
 console.log(literalProxy.goodbye());
+
+const AugmentedProxy = require('./augmentedProxy');
+const augmentedProxy = AugmentedProxy(subject);
+
+console.log("Printing results from augmented proxy...");
+console.log(augmentedProxy.hello());
+console.log(augmentedProxy.goodbye());
